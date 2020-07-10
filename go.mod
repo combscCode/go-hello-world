05acdbcf4fca51b82165129024a23ee77326d00d
Chris Combs <combsc@umich.edu>
2020-07-10 12:57:55 -0400

    Add remote import

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/combscCode/go-hello-world
 
 go 1.14
+
+require github.com/google/go-cmp v0.5.0
--- hello.go
+++ hello.go
@@ -3,9 +3,12 @@ package main
 import (
 		"fmt"
 		"github.com/combscCode/go-hello-world/morestrings"
+
+		"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 		fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+		fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
 
